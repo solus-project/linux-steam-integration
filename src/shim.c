e6d99cf9f0e405a6f099ff985791b657f2649ad8
Ikey Doherty <ikey@solus-project.com>
2016-05-26 21:29:57 +0100

    Add the initial non-functional steam shim
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/shim.c
@@ -0,0 +1,19 @@
+/*
+ * This file is part of linux-steam-integration.
+ *
+ * Copyright (C) 2016 Ikey Doherty
+ *
+ * linux-steam-integration is free software; you can redistribute it and/or
+ * modifyit under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+
+#define __lsi_unused__ __attribute__ ((unused))
+
+int main(__lsi_unused__ int argc, __lsi_unused__ char **argv)
+{
+        return EXIT_FAILURE;
+}
