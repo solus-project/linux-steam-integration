e41c1f1fab6678f78db9c7e53f8fb8c7928356a0
Ikey Doherty <ikey@solus-project.com>
2016-05-26 21:34:28 +0100

    Clean up - add modelines
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/shim.c
+++ src/shim.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2016 Ikey Doherty
  *
  * linux-steam-integration is free software; you can redistribute it and/or
- * modifyit under the terms of the GNU Lesser General Public License as
+ * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
  */
@@ -17,3 +17,16 @@ int main(__lsi_unused__ int argc, __lsi_unused__ char **argv)
 {
         return EXIT_FAILURE;
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
