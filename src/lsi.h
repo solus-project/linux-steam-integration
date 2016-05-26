2eafb39de94711a97d45bcf73ce8c26c3a1237ce
Ikey Doherty <ikey@solus-project.com>
2016-05-26 21:38:22 +0100

    Begin LSI implementation with a 64-bit detection routine
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/lsi.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of linux-steam-integration.
+ *
+ * Copyright (C) 2016 Ikey Doherty
+ *
+ * linux-steam-integration is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+
+/**
+ * Determine if the host system is 64-bit.
+ */
+bool lsi_system_is_64bit(void);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
