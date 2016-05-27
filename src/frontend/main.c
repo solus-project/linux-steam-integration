2350109bb72dd1d289a333dc89fde16f31eb6259
Ikey Doherty <ikey@solus-project.com>
2016-05-27 02:47:08 +0100

    Add non functioning frontend
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/frontend/main.c
@@ -0,0 +1,33 @@
+/*
+ * This file is part of linux-steam-integration.
+ *
+ * Copyright © 2016 Ikey Doherty
+ *
+ * linux-steam-integration is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <gtk/gtk.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+        gtk_init(&argc, &argv);
+
+        return EXIT_FAILURE;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
