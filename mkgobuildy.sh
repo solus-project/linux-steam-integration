29081c492d46aec2ebfd50cfe83c9eea58aa855c
Ikey Doherty <ikey@solus-project.com>
2017-10-10 20:32:52 +0100

    Make build script a bit more robust..
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkgobuildy.sh
+++ mkgobuildy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-CC="gcc -m32" linux32 meson buildroot/build32 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib32
-meson buildroot/build64 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib64
+if [[ ! -d buildroot/build32 ]]; then
+    CC="gcc -m32" linux32 meson buildroot/build32 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib32
+fi
+
+if [[ ! -d buildroot/build64 ]]; then
+    meson buildroot/build64 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib64
+fi
 
 ninja -C buildroot/build32
 ninja -C buildroot/build64
