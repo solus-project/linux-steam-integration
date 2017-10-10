1db376fa38f6eef90afc842545c5e955afe4e800
Ikey Doherty <ikey@solus-project.com>
2017-10-10 20:45:36 +0100

    Turns out fatal is bad always because Steam is bad. Also, preload again
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 unset XMODIFIERS
-export DBUS_FATAL_WARNINGS=1
+export DBUS_FATAL_WARNINGS=0
+export LD_PRELOAD="/usr/\$LIB/libxcb.so.1:/usr/\$LIB/libX11.so.6:/usr/\$LIB/libstdc++.so.6"
 LD_AUDIT="`pwd`/buildroot/install/usr/\$LIB/liblsi-intercept.so" STEAM_RUNTIME=0 /usr/lib64/steam/steam
