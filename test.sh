f2bbe963b69603458c652bb672ec059291fa27b5
Ikey Doherty <ikey@solus-project.com>
2017-10-10 20:52:12 +0100

    Clean up script so LD_AUDIT is a thing again
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
-unset XMODIFIERS
-export DBUS_FATAL_WARNINGS=0
-export LD_PRELOAD="/usr/\$LIB/libxcb.so.1:/usr/\$LIB/libX11.so.6:/usr/\$LIB/libstdc++.so.6"
-env -i DBUS_SESSION_ADDRESS=$DBUS_SESSION_ADDRESS PATH=$PATH DISPLAY=$DISPLAY HOME=$HOME USER=$USER USERNAME=$USERNAME LD_AUDIT="`pwd`/buildroot/install/usr/\$LIB/liblsi-intercept.so" STEAM_RUNTIME=0 /usr/lib64/steam/steam
+env -i DBUS_SESSION_ADDRESS=$DBUS_SESSION_ADDRESS \
+       PATH=$PATH \
+       DISPLAY=$DISPLAY \
+       HOME=$HOME \
+       USER=$USER \
+       USERNAME=$USERNAME \
+       LD_AUDIT="`pwd`/buildroot/install/usr/\$LIB/liblsi-intercept.so" \
+       LD_PRELOAD="/usr/\$LIB/libxcb.so.1:/usr/\$LIB/libX11.so.6:/usr/\$LIB/libstdc++.so.6" \
+       DBUS_FATAL_WARNINGS=0 \
+       STEAM_RUNTIME=0 /usr/lib64/steam/steam
