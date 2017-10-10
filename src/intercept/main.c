d870577a5521c1739ff538bb1f7ee66e22f6007c
Ikey Doherty <ikey@solus-project.com>
2017-10-10 22:07:15 +0100

    intercept: Add missing modelines
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/intercept/main.c
+++ src/intercept/main.c
@@ -149,3 +149,16 @@ _nica_public_ char *la_objsearch(const char *name, __lsi_unused__ uintptr_t *coo
 
         return (char *)name;
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
