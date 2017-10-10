4547d8582afdec189551ee5960de07f995f7252b
Ikey Doherty <ikey@solus-project.com>
2017-10-10 20:50:50 +0100

    One more test to ensure this isnt environment related
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ set -e
 unset XMODIFIERS
 export DBUS_FATAL_WARNINGS=0
 export LD_PRELOAD="/usr/\$LIB/libxcb.so.1:/usr/\$LIB/libX11.so.6:/usr/\$LIB/libstdc++.so.6"
-LD_AUDIT="`pwd`/buildroot/install/usr/\$LIB/liblsi-intercept.so" STEAM_RUNTIME=0 /usr/lib64/steam/steam
+env -i DBUS_SESSION_ADDRESS=$DBUS_SESSION_ADDRESS PATH=$PATH DISPLAY=$DISPLAY HOME=$HOME USER=$USER USERNAME=$USERNAME LD_AUDIT="`pwd`/buildroot/install/usr/\$LIB/liblsi-intercept.so" STEAM_RUNTIME=0 /usr/lib64/steam/steam
