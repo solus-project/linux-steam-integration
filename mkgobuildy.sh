4d3abc5e3721ccf004d8bbc91e0e69321e4ef48e
Ikey Doherty <ikey@solus-project.com>
2017-10-10 22:52:45 +0100

    Enable GTK frontend, don't build Shim for 32-bit
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkgobuildy.sh
+++ mkgobuildy.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [[ ! -d buildroot/build32 ]]; then
-    CC="gcc -m32" linux32 meson buildroot/build32 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib32
+    CC="gcc -m32" linux32 meson buildroot/build32 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib32 -Dwith-shim=none
 fi
 
 if [[ ! -d buildroot/build64 ]]; then
-    meson buildroot/build64 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib64
+    meson buildroot/build64 --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib64 -Dwith-shim=replacement -Dwith-steam-binary=/usr/lib64/steam/steam -Dwith-frontend=true
 fi
 
 ninja -C buildroot/build32
