5ceacb27fb80e4396f46aec229a452cf9359d28b
Ikey Doherty <ikey@solus-project.com>
2017-10-11 16:26:55 +0100

    Post-update mkrelease script
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="0.4"
+VERSION="0.4.1"
 NAME="linux-steam-integration"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
